0c702b08c6806bedb909dd9becbd515b7091fc0a
Pham-Ngoc-Huy <huypham10122001@gmail.com>
2025-01-04 10:17:05 +0700

    Updated

---
+++ 7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void length_of_array(int *num){
+    
+}
+
+int main(){
+    int num,space;
+    cin>>num>>space;
+    if (num >= 1 && num <= 1000){
+        for(int i=0; i <= num; i++){
+
+    }
+    }
+    
+    return 0;
+}
